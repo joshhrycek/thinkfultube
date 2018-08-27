bcf8910ce5dccce8353ad54d11aef474ca0f0e84
Josh Hrycek <joshhrycek@gmail.com>
2018-08-27 18:36:40 -0400

    basic form and submit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello")
+function getVidInfo() {
+    $('.submitButton').on('click',event => {
+        event.preventDefault();
+        let vidTitle = $('.textbox').val();
+        console.log(vidTitle)
+        });
+    }
+
+function generateListItem(){
+    return ``
+}
+
+function renderResults() {
+
+}
+
+function createList(){
+    getVidInfo()
+}
+
+$(createList)
